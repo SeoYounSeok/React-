022fb655b5542dc62fd4948e204b3dc5fe097b37
SeoYounSeok <syounseok94@gmail.com>
2020-09-24 18:39:18 +0900

    # reduce 전

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -5,6 +5,7 @@ function Hello({ color, name, isSpecial }) {
     // True , False 를 나타내는 가장 기본적인 방법은 삼항 연산자
     // {/* { isSpecial ? <b>*</b> : null } */}
     // 밑에의 경우에는 굉장히 간단하기 때문에 단축 평가 논리 계산법을 사용!
+    // Tip1. {isSpecial ? '스페셜하다' : '낫 스페셜하다.'}
     
     <div style={{ color }}>
       {isSpecial && <b>*</b>}
